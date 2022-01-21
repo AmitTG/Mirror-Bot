3bb8fc7cd06e44f0bb5d4da239c57c12c3decd4d
AmitTG <63894720+AmitTG@users.noreply.github.com>
2022-01-22 01:15:43 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -13,13 +13,13 @@ URL_REGEX = r"(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-?=%.]+"
 
 
 class MirrorStatus:
-    STATUS_UPLOADING = "Uploading"
-    STATUS_DOWNLOADING = "Downloading"
+    STATUS_UPLOADING = "Uploading... "
+    STATUS_DOWNLOADING = "Downloading... "
     STATUS_WAITING = "Queued"
     STATUS_FAILED = "Failed. Cleaning download"
     STATUS_CANCELLED = "Cancelled"
-    STATUS_ARCHIVING = "Archiving"
-    STATUS_EXTRACTING = "Extracting"
+    STATUS_ARCHIVING = "Archiving... "
+    STATUS_EXTRACTING = "Extracting... "
 
 
 PROGRESS_MAX_SIZE = 100 // 8
@@ -92,19 +92,20 @@ def get_readable_message():
     with download_dict_lock:
         msg = ""
         for download in list(download_dict.values()):
-            msg += f"<i>{download.name()}</i> - "
+            msg += f"<b>File Name:</b> <i>{download.name()}</i>\n\n"
             msg += download.status()
             if download.status() != MirrorStatus.STATUS_ARCHIVING and download.status() != MirrorStatus.STATUS_EXTRACTING:
-                msg += f"\n<code>{get_progress_bar_string(download)} {download.progress()}</code> of " \
-                       f"{download.size()}" \
-                       f" at {download.speed()}, ETA: {download.eta()} "
+                msg += f"<code>{get_progress_bar_string(download)}</code>\n<b>Progress:</b> <code>{download.progress()}</code>" \
+                       f"<b>Size:</b> <code>{download.size()}</code>\n" \
+                       f"<b>Speed:</b> <code>{download.speed()}</code>\n<b>ETA:</b> <code>{download.eta()}</code>"
             if download.status() == MirrorStatus.STATUS_DOWNLOADING:
                 if hasattr(download, 'is_torrent'):
-                    msg += f"| P: {download.aria_download().connections} " \
-                           f"| S: {download.aria_download().num_seeders}"
-                msg += f"\nGID: <code>{download.gid()}</code>"
+                    msg += f"\n<b>P:</b> {download.aria_download().connections} " \
+                           f"| <b>S:</b> {download.aria_download().num_seeders}"
+                msg += f"\n\n<code>/cancel {download.gid()}</code>"
             msg += "\n\n"
         return msg
+
 
 
 def get_readable_time(seconds: int) -> str:
